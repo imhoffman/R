ea9e39af6c7b24977bf25674285f9a9abe79700c
imhoffman <hoffman.ian.m@gmail.com>
2015-12-22 07:47:35 -0800

    fix plot text display

--- Bennett/pressure.R
+++ Bennett/pressure.R
@@ -8,10 +8,10 @@ KCON$IMHPressurePa <- as.numeric(KCON$StationPressure)*3386.39
 #plot(KCON$IMHDateTime,KCON$IMHPressurePa,xlab="Day in 2010",ylab="Pressure (Pa)",col=2,pch=-1)
 h <- hist(na.omit(KCON$IMHPressurePa),freq=FALSE,col='grey')
 h$density <- h$counts/sum(h$counts)*100
-plot(h,freq=FALSE,ylab="Percentage of Measurements",xlab="Atmospheric Pressure (Pa)", main="Histogram of Hourly Pressure Measurements",sub="Concord, NH, USA, 2010 Feb--Nov",col='grey')
+plot(h,freq=FALSE,ylab="Percentage of Measurements (%)",xlab="Atmospheric Pressure (Pa)", main="Histogram of Hourly Pressure Measurements",sub="Concord, NH, USA, 2010 Feb--Nov",col='grey')
 x <- na.omit(KCON$IMHPressurePa)
-text(min(x),y=25,paste("mean =",floor(mean(x)),"Pa"),pos=4,offset=0.0)
-text(min(x),y=23,paste("stdev =",floor(sd(x)),"Pa"),pos=4,offset=0.0)
+text(0.25*(max(x)-min(x))+min(x),y=25,paste("mean =",floor(mean(x)),"Pa"),pos=2,offset=0.0)
+text(0.25*(max(x)-min(x))+min(x),y=23,paste("stdev =",floor(sd(x)),"Pa"),pos=2,offset=0.0)
 #xfit <- seq(min(x),max(x),length=128)
 #yfit <- dnorm(xfit,mean=mean(x),sd=sd(x))
 #lines(xfit,yfit,col=3,lwd=2)
